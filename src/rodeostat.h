55ec597bbf2cd413eaab86831fdb18a9c9e9d1b1
benschaser <benschaser@gmail.com>
2024-02-17 16:28:42 -0600

    init dev branch

--- src/rodeostat.h
+++ src/rodeostat.h
@@ -10,8 +10,9 @@ public:
     Control control;
     Graphics graphics;
     std::vector<Contestant> contestants;
+    int current_contestant{0};
 
-    
+    void update_frame();
 private:
     Settings settings;
 
